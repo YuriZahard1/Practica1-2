d6fc579e0da64f5705c5e7ef4744f720b28417e5
Christian <c1s2anchez@gmail.com>
2026-09-22 19:24:45 +0200

    Console.log

--- Ej2.js
+++ Ej2.js
@@ -4,3 +4,6 @@ const impactoCritico = (danoBase, multiplicador) => danoBase * multiplicador;
 // (Usa retorno implícito de dos parámetros).
 const mensajeAlerta = () => "¡Alerta: Intrusos en la cubierta!";
 // (Usa arrow function sin parámetros).
+console.log(calcularEscudo(10));
+console.log(impactoCritico(10, 10));
+console.log(mensajeAlerta());
